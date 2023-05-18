8c8079669c937dee8b833785a9c4a2a5ba672159
Auntor <mmhauntor@gmail.com>
2023-05-18 22:08:21 +0600

    new updated fixed error

--- Django/AiPro/apptest/serializers.py
+++ Django/AiPro/apptest/serializers.py
@@ -16,12 +16,3 @@ class Client4Serializer(TokenObtainPairSerializer):
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
     
-    def validate(self, attrs):
-        data= super().validate(attrs)
-        refresh = self.get_token(self.user)
-        data['refresh']=str(refresh)
-        data['access']=str(refresh.access_token)
-        data['type']="Baearer"
-        data['lifetime']=str(settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'].days)+"days"
-
-        return data
